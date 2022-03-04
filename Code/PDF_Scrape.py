55a449d7bfbf27067c7d6a8a3953e7f2a9a4280d
Ethan Powell <epowell50@gmail.com>
2022-03-03 22:33:07 -0500

    Finished PDF prase

--- Code/PDF_Scrape.py
+++ Code/PDF_Scrape.py
@@ -1,3 +1,4 @@
+from posixpath import split
 import pdftotext
 import re
 
@@ -5,15 +6,26 @@ import re
 with open("Test.pdf", "rb") as f:
     pdf = pdftotext.PDF(f)
 
-# This section joins the data into a single string and begins paring it
-# into a useful form through regex and splits
+# Creates a single string of all text
 output = "".join(pdf)
+
+# Splits the string into a tuple
 info_list = output.split("\n")
+
+# Removes trailing and leading whitespace
 info_list = [s.strip() for s in info_list]
+
+# Replaces all "spaced" sections with "-", preserving
+# single spaced pieces
 for i in range(len(info_list)):
-    string = info_list[i]
-    string = re.sub("\s+", " ", string)
-    info_list[i] = string
-#info_list = [s.split(" ") for s in info_list]
-#info_list = [s.replace(" ", "") for s in info_list]
-print(info_list)
+    a = info_list[i]
+    a = re.sub("\s\s+", "_", a)
+    info_list[i] = a
+
+# Creates an empty list to store the new data
+data = []
+for i in range(len(info_list)):
+    a = info_list[i]
+    print(a)
+
+#print(data)
